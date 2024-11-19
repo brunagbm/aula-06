a13f36fa4483de74160cdf47082b04051c3b6bf8
brunagbm <brunamendonaa@gmail.com>
2024-11-19 20:40:02 -0300

    Atividade 2 concluída

---
+++ exerc_2.js
@@ -0,0 +1,10 @@
+let temperature = 45
+
+if (temperature > 30) {
+    console.log("Está quente!")
+
+} else if (temperature <= 30) {
+    console.log("Está frio!")
+
+} else (temperature > 40)
+    console.log("Está muito quente!")
